3d1c8a921c69d550d062a800885e9cc5447401ad
Carla Holcomb <carla.programmer@gmail.com>
2019-11-12 02:02:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_accessor :new, :size, :material, :condition, :brand
+  attr_reader :roster, :new
+
+  CLASS = []
+  ROSTERS = []
+
+  def initialized(new)
+    @new = new
+    @school = []
+  end
+
+  def add_student=(new)
+   @school << new
+   CLASS << new
+   GRADE << new
+ end
+
+
+end
